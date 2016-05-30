98b5ca2ee26733a4cfd8dca3ead23df7758f39d7
Rabinzon <ildarrazin@gmail.com>
2016-05-30 13:25:15 +0300

    feat(resetButton): add props type validation

--- src/components/ResetButton/ResetButton.jsx
+++ src/components/ResetButton/ResetButton.jsx
@@ -3,6 +3,10 @@ import React, { Component, PropTypes as pt} from 'react';
 import styles from './ResetButton.styl';
 
 class ResetButton extends Component {
+  static propTypes = {
+    onChange: pt.func
+  };
+
   render() {
     const { onChange } = this.props;
     return (
