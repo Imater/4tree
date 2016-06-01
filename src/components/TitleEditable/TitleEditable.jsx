e52d5d125ff56ba8d2954fcdcbb7b4841f5c4bfd
Imater <eugene.leonar@gmail.com>
2016-06-01 07:16:48 +0500

    cleanup

--- src/components/TitleEditable/TitleEditable.jsx
+++ src/components/TitleEditable/TitleEditable.jsx
@@ -3,35 +3,35 @@ import React, { Component } from 'react';
 import styles from './TitleEditable.styl';
 
 class TitleEditable extends Component {
-state = {
-  isEdit: false
-};
+  state = {
+    isEdit: false
+  };
 
-render() {
-  const { isEdit } = this.state;
-  return (
-this.state.isEdit ? (
-<input
-onBlur = { () => {
-  this.setState({
-    isEdit: !isEdit
-  });
-}
-}
-/>
-) : (
-<div
-onClick={() => {
-  this.setState({
-    isEdit: !isEdit
-  });
-}}
->
-Some Title
-</div>
-)
-);
-}
+  render() {
+    const { isEdit } = this.state;
+    return (
+      this.state.isEdit ? (
+        <input
+          onBlur = { () => {
+            this.setState({
+              isEdit: !isEdit
+            });
+          }
+          }
+        />
+      ) : (
+        <div
+          onClick={() => {
+            this.setState({
+              isEdit: !isEdit
+            });
+          }}
+        >
+          Some Title
+        </div>
+      )
+    );
+  }
 }
 
 export default TitleEditable;
