833ae6fb4d98772ffb2f598efb0420dce2f9cf8c
Vadim Yakhin <yakhin.v@gmail.com>
2016-06-04 20:30:55 +0300

    refactor accordion to get state from store

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -3,11 +3,13 @@ import { routeReducer as routing } from 'react-router-redux';
 import { reducer as reduxAsyncConnect } from 'redux-async-connect';
 
 import settings from './settings';
+import accordion from './accordion.js';
 
 const reducers = combineReducers({
   reduxAsyncConnect,
   settings,
   routing,
+  accordion
 });
 
 export default reducers;
