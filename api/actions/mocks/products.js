c3e78e009fb1cfbe59324bc5e3292e55daab2788
Imater <eugene.leonar@gmail.com>
2016-08-12 15:44:20 +0500

    add mock file for test

---
+++ api/actions/mocks/products.js
@@ -0,0 +1,407 @@
+const products = {
+  entities: {
+    section: {
+      1349: 'Бумажная продукция',
+      65788: 'Школьное планирование',
+      65789: 'Дневники',
+      65847: 'дневники для младших классов',
+      65846: 'дневники универсальные'
+    },
+    property: {
+      up_tm: {
+        title: 'бренд',
+        primary: 'y',
+        value: {
+          12908290: 'artspace',
+          12820142: 'hatber'
+        }
+      },
+      up_ruler: {
+        title: 'серия',
+        primary: 'y',
+        value: {
+          275026735: 'nici'
+        }
+      },
+      type: {
+        title: 'тип',
+        primary: 'y',
+        value: {
+          1234: 'для младших классов (1-4)',
+          1224: 'универсальные (1-11)'
+        }
+      },
+      count_sheet: {
+        title: 'количество листов',
+        primary: 'y',
+        value: {
+          1235: 48
+        }
+      },
+      type_cover: {
+        title: 'тип обложки',
+        primary: 'y',
+        value: {
+          1236: 'мягкая',
+          1226: 'твердая'
+        }
+      },
+      type_mount: {
+        title: 'тип скрепления',
+        primary: 'y',
+        value: {
+          1237: 'скрепка',
+          1227: 'сшивка'
+        }
+      },
+      cover_material: {
+        title: 'материал обложки',
+        primary: 'y',
+        value: {
+          1228: 'мелованная бумага',
+          1238: 'мелованный картон'
+        }
+      }
+    }
+  },
+  breadcrumb: [
+    {
+      parent: 65847
+    },
+    {
+      NAME: 'Бумажная продукция',
+      LINK: '/c/'
+    },
+    {
+      NAME: 'Бумага для творчества, книги, раскраски',
+      LINK: ''
+    }
+  ],
+  items: {
+    194108769: {
+      id: 194108769,
+      name: 'дневник 1-4 кл. 48л. \'забавный пес\', вд-лак',
+      link: '',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/2f7/246_250_1/43948a81_7b83_11e4_9b34_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/2f7/43948a81_7b83_11e4_9b34_e4115bd714a8.jpg',
+      iblock_section_id: 65847,
+
+      price: 14.59,
+      stock: 4057,
+      in_basket: 0,
+      delay: 'n',
+      compare: 'n',
+      favorite: 'y',
+
+      up_code_1c: '211388',
+      up_article: 'дм48_4726',
+      sort_tm: 0,
+      sort_node: 50,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '12/1/48',
+        step: 1,
+        min: 12
+      },
+
+      property: {
+        up_tm: 128200,
+        type: 1234,
+        count_sheet: 1235,
+        type_cover: 1236,
+        type_mount: 1237,
+        cover_material: 1238
+      }
+    },
+    292806703: {
+      id: 292806703,
+      name: 'дневник 1-4 кл. 48л. \'любимый мишка\' (nici)',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/27e/246_250_1/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/27e/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      iblock_section_id: 65847,
+
+      price: 21.7,
+      stock: 1046,
+      in_basket: 10,
+      delay: 'n',
+      compare: 'n',
+      favorite: 'y',
+
+      up_code_1c: '227501',
+      up_article: '48дм5в_15282',
+      sort_tm: 5,
+      sort_node: 10,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '10/1/40',
+        step: 1,
+        min: 10
+      },
+      property: {
+        up_tm: 12820142,
+        up_ruler: 275026735,
+        type: 1234,
+        count_sheet: 1235,
+        type_cover: 1236,
+        type_mount: 1237,
+        cover_material: 1238
+      }
+    },
+    288080284: {
+      id: 288080283,
+      name: 'дневник',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/27e/246_250_1/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/27e/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      iblock_section_id: 65846,
+
+      price: 39.62,
+      stock: 3337,
+      in_basket: 15,
+      delay: 'n',
+      compare: 'y',
+      favorite: 'n',
+
+      up_code_1c: '226521',
+      up_article: 'ду40т_8324',
+      sort_tm: 5,
+      sort_node: 10,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '5/1/40',
+        step: 1,
+        min: 5
+      },
+      property: {
+        up_tm: 12908290,
+        type: 1224,
+        count_sheet: 1235,
+        type_cover: 1226,
+        type_mount: 1227,
+        cover_material: 1228
+      }
+    },
+    194108771: {
+      id: 194108769,
+      name: 'дневник 1-4 кл. 48л. \'забавный пес\', вд-лак',
+      link: '',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/2f7/246_250_1/43948a81_7b83_11e4_9b34_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/2f7/43948a81_7b83_11e4_9b34_e4115bd714a8.jpg',
+      iblock_section_id: 65847,
+
+      price: 14.59,
+      stock: 4057,
+      in_basket: 0,
+      delay: 'n',
+      compare: 'n',
+      favorite: 'y',
+
+      up_code_1c: '211388',
+      up_article: 'дм48_4726',
+      sort_tm: 0,
+      sort_node: 50,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '12/1/48',
+        step: 1,
+        min: 12
+      },
+      property: {
+        up_tm: 128200,
+        type: 1234,
+        count_sheet: 1235,
+        type_cover: 1236,
+        type_mount: 1237,
+        cover_material: 1238
+      }
+    },
+    292806721: {
+      id: 292806703,
+      name: 'дневник 1-4 кл. 48л. \'любимый мишка\' (nici)',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/27e/246_250_1/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/27e/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      iblock_section_id: 65847,
+
+      price: 21.7,
+      stock: 1046,
+      in_basket: 10,
+      delay: 'n',
+      compare: 'n',
+      favorite: 'y',
+
+      up_code_1c: '227501',
+      up_article: '48дм5в_15282',
+      sort_tm: 5,
+      sort_node: 10,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '10/1/40',
+        step: 1,
+        min: 10
+      },
+      property: {
+        up_tm: 12820142,
+        up_ruler: 275026735,
+        type: 1234,
+        count_sheet: 1235,
+        type_cover: 1236,
+        type_mount: 1237,
+        cover_material: 1238
+      }
+    },
+    288080291: {
+      id: 288080283,
+      name: 'дневник',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/27e/246_250_1/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/27e/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      iblock_section_id: 65846,
+
+      price: 39.62,
+      stock: 3337,
+      in_basket: 15,
+      delay: 'n',
+      compare: 'y',
+      favorite: 'n',
+
+      up_code_1c: '226521',
+      up_article: 'ду40т_8324',
+      sort_tm: 5,
+      sort_node: 10,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '5/1/40',
+        step: 1,
+        min: 5
+      },
+      property: {
+        up_tm: 12908290,
+        type: 1224,
+        count_sheet: 1235,
+        type_cover: 1226,
+        type_mount: 1227,
+        cover_material: 1228
+      }
+    },
+    194108776: {
+      id: 194108769,
+      name: 'дневник 1-4 кл. 48л. \'забавный пес\', вд-лак',
+      link: '',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/2f7/246_250_1/43948a81_7b83_11e4_9b34_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/2f7/43948a81_7b83_11e4_9b34_e4115bd714a8.jpg',
+      iblock_section_id: 65847,
+
+      price: 14.59,
+      stock: 4057,
+      in_basket: 0,
+      delay: 'n',
+      compare: 'n',
+      favorite: 'y',
+
+      up_code_1c: '211388',
+      up_article: 'дм48_4726',
+      sort_tm: 0,
+      sort_node: 50,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '12/1/48',
+        step: 1,
+        min: 12
+      },
+
+      property: {
+        up_tm: 128200,
+        type: 1234,
+        count_sheet: 1235,
+        type_cover: 1236,
+        type_mount: 1237,
+        cover_material: 1238
+      }
+    },
+    292806716: {
+      id: 292806716,
+      name: 'дневник 1-4 кл. 48л. \'любимый мишка\' (nici)',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/27e/246_250_1/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/27e/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      iblock_section_id: 65847,
+
+      price: 21.7,
+      stock: 1046,
+      in_basket: 10,
+      delay: 'n',
+      compare: 'n',
+      favorite: 'y',
+
+      up_code_1c: '227501',
+      up_article: '48дм5в_15282',
+      sort_tm: 5,
+      sort_node: 10,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '10/1/40',
+        step: 1,
+        min: 10
+      },
+      property: {
+        up_tm: 12820142,
+        up_ruler: 275026735,
+        type: 1234,
+        count_sheet: 1235,
+        type_cover: 1236,
+        type_mount: 1237,
+        cover_material: 1238
+      }
+    },
+    288080294: {
+      id: 288080294,
+      name: 'дневник',
+      preview_picture: 'http://relefopt.ru/upload/resize_cache/iblock/27e/246_250_1/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      detail_picture: 'http://relefopt.ru/upload/iblock/27e/d09584fb_d6e6_11e5_ab19_e4115bd714a8.jpg',
+      iblock_section_id: 65846,
+
+      price: 39.62,
+      stock: 3337,
+      in_basket: 15,
+      delay: 'n',
+      compare: 'y',
+      favorite: 'n',
+
+      up_code_1c: '226521',
+      up_article: 'ду40т_8324',
+      sort_tm: 5,
+      sort_node: 10,
+      up_mark: 'n',
+      up_symbol_of_year: 'y',
+      up_hot_products: 'n',
+      up_multiplicity: {
+        html: '5/1/40',
+        step: 1,
+        min: 5
+      },
+      property: {
+        up_tm: 12908290,
+        type: 1224,
+        count_sheet: 1235,
+        type_cover: 1226,
+        type_mount: 1227,
+        cover_material: 1228
+      }
+    }
+  }
+};
+
+export default products;
